09773a7b8aa99e366758ab6197e610be1ac3aad5
Hun-0322 <ksh26271@naver.com>
2022-12-07 16:01:18 +0900

    TableView 코드로 구현

---
+++ TableViewCode/TableViewCode/Models/Movie.swift
@@ -0,0 +1,14 @@
+//
+//  Movie.swift
+//  TableViewCode
+//
+//  Created by KSH on 2022/12/06.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+}
